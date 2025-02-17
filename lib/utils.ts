52aed59910ba6205d6e01a03eb6401b4b5f06345
Efosa Igbinehi <liova2273@gmail.com>
2025-02-17 18:10:27 +0000

    imported both ClassValue type and clsx function from 'clsx'

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,6 @@
-import { type ClassValue } from 'clsx';
+import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(...inputs);
+  return twMerge(clsx(...inputs));
 }
