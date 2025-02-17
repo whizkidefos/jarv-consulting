c66c6e2a9e4127385e1cd07ffa78072a0582a710
Efosa Igbinehi <liova2273@gmail.com>
2025-02-17 18:27:21 +0000

    added custom favicon SVG

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,35 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'JARV Consulting - Adult Social Care & Supported Living',
   description: 'CQC-registered Adult Social Care and Supported Living company providing comfortable accommodation and support services.',
+  icons: {
+    icon: [
+      {
+        url: '/favicon.svg',
+        type: 'image/svg+xml',
+      },
+    ],
+  },
+  authors: [
+    {
+      name: 'JARV Consulting',
+      url: 'https://jarvconsulting.co.uk',
+    },
+  ],
+  keywords: [
+    'Supported Living',
+    'Adult Social Care',
+    'CQC Registered',
+    'Care Services',
+    'Bolton',
+    'Greater Manchester',
+  ],
+  creator: 'JARV Consulting',
+  publisher: 'JARV Consulting',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
 };
 
 export default function RootLayout({
@@ -18,6 +47,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/favicon.svg"
+          sizes="any"
+        />
+      </head>
       <body className={inter.className} suppressHydrationWarning>
         <div className="flex min-h-screen flex-col">
           <Navigation />
